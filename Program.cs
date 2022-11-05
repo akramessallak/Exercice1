6feac03c554b12ac04bee38df4fb890e9df5f3ca
mitskiシ <mitski@DESKTOP-KIMO>
2022-11-05 23:41:00 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice1
+{
+    public class Program
+    {
+        public static bool EstPremier(int p)
+        {
+            if (p < 2)
+            {
+                return true;
+            }
+            else
+            {
+                for (int i = 2; i < p; i++)
+                {
+                    if (p % i == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+
+
+            int p = 0;
+            do
+            {
+                Console.WriteLine("Veuillez entrez un nombre :");
+
+            } while (!int.TryParse(Console.ReadLine(), out p));
+
+            if (EstPremier(p))
+            {
+                Console.WriteLine("Le nombre est premier");
+            }
+            else
+            {
+                Console.WriteLine("Le nombre n'est premier");
+            }
+
+
+        }
+    }
+}
